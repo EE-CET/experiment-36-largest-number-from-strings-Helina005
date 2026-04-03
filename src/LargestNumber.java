062cab04fd423734f1d50d44d60ae6f0ad4835fc
Helina Alex <helinatalex@gmail.com>
2026-04-03 16:38:22 +0530

    largestnumber

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -1,14 +1,47 @@
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Comparator;
+
+   
+        
+    import java.util.*;
 
 public class LargestNumber {
-   
-        // TODO: Read N
-        // TODO: Read N strings into an array
-        // TODO: Sort the strings using a custom comparator
-        // Comparator logic: (str1, str2) -> (str2 + str1).compareTo(str1 + str2)
-        // TODO: Handle edge case where all numbers are "0" (e.g. input 0 0 -> output "0")
-        // TODO: Concatenate sorted strings and print
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read N
+        if (!sc.hasNextInt()) return;
+        int n = sc.nextInt();
+
+        // Read the N strings
+        String[] nums = new String[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.next();
+        }
+
+        // Sort using a custom comparator
+        Arrays.sort(nums, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                // Compare (b + a) with (a + b) to get descending order
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1);
+            }
+        });
+
+        // Special case: If the largest number is "0", the entire result is "0"
+        if (nums[0].equals("0")) {
+            System.out.println("0");
+            return;
+        }
+
+        // Combine the strings
+        StringBuilder sb = new StringBuilder();
+        for (String s : nums) {
+            sb.append(s);
+        }
+
+        System.out.println(sb.toString());
+        sc.close();
+    }
+
 }
